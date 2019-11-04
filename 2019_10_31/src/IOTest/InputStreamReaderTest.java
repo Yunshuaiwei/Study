d52d946e76d8d61d7be358eec36f3d4e1e812ea7
Yunshuaiwei <1779001867@qq.com>
2019-11-04 12:21:19 +0800

    IO流初学

---
+++ 2019_10_31/src/IOTest/InputStreamReaderTest.java
@@ -0,0 +1,92 @@
+package IOTest;
+
+import org.junit.Test;
+
+import java.io.*;
+
+/**
+ * @ProjectName 2019_10_31
+ * @ClassName InputStreamReader
+ * Description
+ * @Auther YunSW
+ * @Date 2019/11/4 10:18
+ * @Version 1.0
+ *
+ * 处理流二：转换流：属于字符流
+ *      InputStreamReader：将一个字节的输入流转换为字符的书输入流
+ *      OutputStreamWriter：将一个字符的输出流转换为字节的输出流
+ * 作用：提供字节流与字节流的相互转换
+ *3、解码：字节、字节数组----->字符数组、字符串
+ *   编码：字符数组、字符串---->字节、字节数组
+ *4、字符集：
+ **/
+public class InputStreamReaderTest {
+    @Test
+    public void test1() {
+        InputStreamReader isr= null;
+        try {
+            FileInputStream fis=new FileInputStream("hello1");
+            isr = new InputStreamReader(fis,"UTF-8");//默认为文件保存时的字符集
+
+            char [] cbuf=new char[5];
+            int len;
+            while((len=isr.read(cbuf))!=-1){
+                String str=new String(cbuf,0,len);
+                System.out.print(str);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if(isr!=null){
+                    isr.close();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+    /**
+     * 综合使用InputStreamReader和OutputStreamWriter
+     */
+    @Test
+    public void teat2(){
+        //1、造文件、造流
+        File file1=new File("hello1");
+        File file2=new File("hello_gbk");
+        InputStreamReader isr= null;
+        OutputStreamWriter osw= null;
+        try {
+            FileInputStream fis=new FileInputStream(file1);
+            FileOutputStream fos=new FileOutputStream(file2);
+
+            isr = new InputStreamReader(fis,"utf-8");
+            osw = new OutputStreamWriter(fos,"gbk");
+            //2、读写过程
+            char[] cbuf=new char[5];
+            int len;
+            while((len=isr.read(cbuf))!=-1){
+                osw.write(cbuf,0,len);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            //3、资源关闭
+            try {
+                if(isr!=null){
+                    isr.close();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            try {
+                if(osw!=null){
+                    osw.close();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+    }
+}
---
+++ 2019_10_31/src/IOTest/OtherSreamTest.java
@@ -0,0 +1,55 @@
+package IOTest;
+
+import org.junit.Test;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.sql.SQLOutput;
+
+/**
+ * @ProjectName 2019_10_31
+ * @ClassName OtherSreamTest
+ * Description
+ * @Auther YunSW
+ * @Date 2019/11/4 11:27
+ * @Version 1.0
+ **/
+public class OtherSreamTest {
+    /**
+     * 1、标准的输入、输出流
+     * System.in：标准的输入流，默认从键盘输入
+     * System.out：标准的输出流默认从控制台输出
+     * 2、System类的setIn()/setOut()方式重新指定输入和输出的流
+     */
+    public static void main(String[] args) {
+        test1();
+    }
+    public static void test1() {
+        BufferedReader br = null;
+        try {
+            InputStreamReader isr = new InputStreamReader(System.in);
+            br = new BufferedReader(isr);
+            while (true) {
+                System.out.println("请输入字符串：");
+                String data = br.readLine();
+                if (data.equalsIgnoreCase("e") || data.equalsIgnoreCase("exit")) {
+                    System.out.println("程序退出！");
+                    break;
+                }
+                String upperCase = data.toUpperCase();
+                System.out.println(upperCase);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (br != null) {
+                try {
+                    br.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+}
