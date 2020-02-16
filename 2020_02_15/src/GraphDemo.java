9159b427bd66022cd4f838c4036aa4bad5f9a5f1
Yunshuaiwei <1779001867@qq.com>
2020-02-16 12:10:37 +0800

    图

--- 2020_02_15/src/GraphDemo.java
+++ 2020_02_15/src/GraphDemo.java
@@ -20,6 +20,8 @@ public class GraphDemo {
         graph.insertEdges(1, 4, 1);
 
         graph.showGraph();
+        System.out.println("深度优先遍历：");
+        graph.dfs();
     }
 }
 
@@ -27,11 +29,77 @@ class Graph {
     private ArrayList<String> vertexList;
     private int[][] edges;
     private int numOfEdges;
+    private boolean[] isVisited;
 
     public Graph(int n) {
         edges = new int[n][n];
         vertexList = new ArrayList<>(n);
         numOfEdges = 0;
+        isVisited = new boolean[n];
+    }
+
+    /**
+     * 判断它的邻接节点
+     *
+     * @param index 需要查找的下标
+     * @return
+     */
+    public int getFirstNeighbor(int index) {
+        for (int i = 0; i < vertexList.size(); i++) {
+            if (edges[index][i] > 0) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    /**
+     * 根据前一个邻接节点的下标获取下一个邻接节点
+     *
+     * @param v1
+     * @param v2
+     * @return
+     */
+    public int getNextNeighbor(int v1, int v2) {
+        for (int i = v2 + 1; i < vertexList.size(); i++) {
+            if (edges[v1][i] > 0) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    /**
+     * 深度优先遍历
+     *
+     * @param isVisited 标记是否被访问过的数组
+     * @param i         开始访问的下标
+     */
+    private void dfs(boolean[] isVisited, int i) {
+        //首先访问该节点
+        System.out.print(getValueByIndex(i) + "->");
+        //将节点标记为已访问
+        isVisited[i] = true;
+        //查找节点i的跪一个邻接节点
+        int w = getFirstNeighbor(i);
+        while (w != -1) {//说明有邻接节点
+            if (!isVisited[w]) {
+                dfs(isVisited, w);
+            }
+            //如果w节点被访问过
+            w = getNextNeighbor(i, w);
+        }
+    }
+
+    /**
+     * 对dfs进行重载
+     */
+    public void dfs() {
+        for (int i = 0; i < getNumOfVertex(); i++) {
+            if (!isVisited[i]) {
+                dfs(isVisited, i);
+            }
+        }
     }
 
     public int getNumOfVertex() {
