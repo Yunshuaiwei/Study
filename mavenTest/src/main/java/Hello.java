dc72554e6a8f20a3fa023fd5b3e625d560232254
ysw <1779001867@qq.com>
2020-06-13 22:08:40 +0800

    学习

---
+++ mavenTest/src/main/java/Hello.java
@@ -0,0 +1,11 @@
+/**
+ * @Description TODO
+ * @Author YunShuaiWei
+ * @Date 2020/6/13 16:00
+ * @Version
+ **/
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello");
+    }
+}
---
+++ mavenTest/src/test/java/Test.java
@@ -0,0 +1,11 @@
+/**
+ * @Description TODO
+ * @Author YunShuaiWei
+ * @Date 2020/6/13 16:07
+ * @Version
+ **/
+public class Test {
+    public void test(){
+        System.out.println();
+    }
+}
