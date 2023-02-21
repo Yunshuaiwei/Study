98a124e64006cceef9b8b8b4269840133c6c7a94
贠帅伟 <1779001867@qq.com>
2023-02-21 21:55:09 +0800

    add:复习

--- SortingAlgorithm/src/Solution.java
+++ SortingAlgorithm/src/Solution.java
@@ -453,8 +453,151 @@ public class Solution {
      * @date 2023/2/20 15:23
      **/
     public int maxProfit(int[] prices) {
+        int cost = Integer.MAX_VALUE, profit = 0;
+        for (int price : prices) {
+            cost = Math.min(cost, price);
+            profit = Math.max(profit, price - cost);
+        }
+        return profit;
+    }
+
+    /**
+     * 剑指 Offer 42. 连续子数组的最大和
+     *
+     * @author yunshuaiwei
+     * @date 2023/2/21 16:09
+     **/
+    public int maxSubArray(int[] nums) {
+        int res = nums[0];
+        for (int i = 1; i < nums.length; i++) {
+            nums[i] += Math.max(nums[i - 1], 0);
+            res = Math.max(res, nums[i]);
+        }
+        return res;
+    }
+
+    /**
+     * 剑指 Offer 47. 礼物的最大价值
+     *
+     * @author yunshuaiwei
+     * @date 2023/2/21 16:24
+     **/
+    public int maxValue(int[][] grid) {
 
         return 0;
+    }
+
+    /**
+     * 剑指 Offer 46. 把数字翻译成字符串
+     *
+     * @author yunshuaiwei
+     * @date 2023/2/21 21:04
+     **/
+    public int translateNum(int num) {
+
+        return 0;
+    }
+
+    /**
+     * 剑指 Offer 18. 删除链表的节点
+     *
+     * @author yunshuaiwei
+     * @date 2023/2/21 21:06
+     **/
+    public ListNode deleteNode(ListNode head, int val) {
+        ListNode cur = head;
+        ListNode next = cur.next;
+        if (cur.val == val) {
+            return next;
+        }
+        while (next != null) {
+            if (next.val == val) {
+                cur.next = next.next;
+            }
+            cur = cur.next;
+            next = next.next;
+        }
+        return head;
+    }
+
+    /**
+     * 剑指 Offer 22. 链表中倒数第k个节点
+     *
+     * @author yunshuaiwei
+     * @date 2023/2/21 21:14
+     **/
+    public ListNode getKthFromEnd(ListNode head, int k) {
+        ListNode slow = head;
+        ListNode quick = head;
+        while (k > 0) {
+            quick = quick.next;
+            k--;
+        }
+        while (quick != null) {
+            slow = slow.next;
+            quick = quick.next;
+        }
+        return slow;
+    }
+
+    /**
+     * 剑指 Offer 25. 合并两个排序的链表
+     *
+     * @author yunshuaiwei
+     * @date 2023/2/21 21:35
+     **/
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        if (l1 == null || l2 == null) {
+            return l1 == null ? l2 : l1;
+        }
+        ListNode cur1 = l1;
+        ListNode cur2 = l2;
+        ListNode newNode = null;
+        ListNode next = null;
+        while (cur1 != null && cur2 != null) {
+            if (cur1.val <= cur2.val) {
+                if (newNode == null) {
+                    newNode = new ListNode(cur1.val);
+                    next = newNode;
+                } else {
+                    next.next = new ListNode(cur1.val);
+                    next = next.next;
+                }
+                cur1 = cur1.next;
+            } else {
+                if (newNode == null) {
+                    newNode = new ListNode(cur2.val);
+                    next = newNode;
+                } else {
+                    next.next = new ListNode(cur2.val);
+                    next = next.next;
+                }
+                cur2 = cur2.next;
+            }
+        }
+        if (cur1 == null) {
+            next.next = cur2;
+        }
+        if (cur2 == null) {
+            next.next = cur1;
+        }
+        return newNode;
+    }
+
+    /**
+     * 剑指 Offer 52. 两个链表的第一个公共节点
+     *
+     * @author yunshuaiwei
+     * @date 2023/2/21 21:45
+     **/
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        ListNode cur1 = headA;
+        ListNode cur2 = headB;
+        while (cur1 != cur2) {
+            cur1 = cur1 == null ? headB : cur1.next;
+            cur2 = cur2 == null ? headA : cur2.next;
+        }
+        return cur2;
     }
 
     public static void main(String[] args) {
